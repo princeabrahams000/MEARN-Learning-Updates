10d60635d65469addff1fb1cb0d92adb128546e4
princeabrahams000 <princeabrahams000@gmail.com>
2024-04-29 21:14:00 +0530

    29/4

--- JavaScript/prog74.js
+++ JavaScript/prog74.js
@@ -1,2 +1,25 @@
 array = [10,20,30,40,30,20,50,30,60,70,10,40,80]
 //write a programme to find the number count from the given array? and display as an object.
+
+obj = {}
+array.forEach(item=>item in obj?obj[item]+=1:obj[item]=1);
+console.log(obj);
+
+console.log('-----------------------');
+
+pattern = 'ABCBBCAACB'
+// w.a.p to find the first recursive letter - B
+
+obj1 ={}
+
+character = Array.from(pattern)
+
+for (char of character){
+    if(char in obj1){
+        console.log(char);
+        break
+    }
+    else{
+        obj1[char]=1
+    }
+}
---
+++ JavaScript/prog75.js
@@ -0,0 +1,12 @@
+weatherData=[
+    {district:'Thrissur',weather:32},
+    {district:'Kottayam',weather:29},
+    {district:'Palakkad',weather:34},
+    {district:'Ernakulam',weather:33},
+    {district:'Thrissur',weather:29},
+    {district:'Kottayam',weather:30},
+    {district:'Palakkad',weather:32},
+    {district:'Ernakulam',weather:31}
+]
+//print district with its highest temparature
+//output: {Thrissur:32, Kottayam:30,Palakkad:34,Ernakulam:33}
