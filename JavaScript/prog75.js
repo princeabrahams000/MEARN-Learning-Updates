85b82ddd8f0bbedb27f49d2254cd119258757687
princeabrahams000 <princeabrahams000@gmail.com>
2024-05-02 01:36:53 +0530

    02/5/24-1.36AM

--- JavaScript/prog75.js
+++ JavaScript/prog75.js
@@ -10,3 +10,31 @@ weatherData=[
 ]
 //print district with its highest temparature
 //output: {Thrissur:32, Kottayam:30,Palakkad:34,Ernakulam:33}
+
+
+
+// 1) create output object
+wd = {}
+
+// 2) access each item from the WeatherData array
+for (data of weatherData){
+    district = data.district
+    currTemp = data.weather
+
+    // 3) check district is there in the output object
+    if(district in wd){
+        oddTemp = wd[district]
+        //4) compare the temperature
+        if(oddTemp>currTemp){
+            wd[district]=oddTemp
+        }
+        else{ //if new value is greater than old value
+            wd[district] = currTemp
+        }
+    }
+    else{ //if district not present in output object
+        wd[district] = currTemp
+    }
+}
+
+console.log(wd);
---
+++ JavaScript/prog76.js
@@ -0,0 +1,19 @@
+products=[
+    {pid:100,pName:'apple',band:'5g',price:120000,display:'led'},
+    {pid:101,pName:'samsaung',band:'5g',price:45000,display:'led'},
+    {pid:102,pName:'blackberry',band:'4g',price:50000,display:'led'},
+    {pid:103,pName:'nokia',band:'3g',price:1200,display:'lcd'},
+    {pid:104,pName:'motorola',band:'4g',price:10000,display:'lcd'}
+]
+
+//1. print product name only
+
+//2. print all mobile details whose display is lcd
+
+//3. print 5g mobile phone name
+
+//4. sort mobile based on price
+
+//5. print costly mobile
+
+//6. print low cost mobile
