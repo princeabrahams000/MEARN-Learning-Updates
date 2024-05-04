2064d9c390b78c83c04375bde405fba719bbe61d
princeabrahams000 <princeabrahams000@gmail.com>
2024-05-04 23:33:14 +0530

    4/5/2024-11.33pm

--- JavaScript/prog80.js
+++ JavaScript/prog80.js
@@ -10,15 +10,80 @@ class Bank{
     }
     //validate a given account number
     validation(acno){
-        return acno in this.accountDetails?'Account is valid':'invalid account'
+        return acno in this.accountDetails?true:false
     }
 
     //authenticate the account
+    authentice(acno,pswd){
+         if(this.validation(acno)){
+            if(this.accountDetails[acno].password == pswd){
+                console.log('authentication successful');
+            }
+         }
+         else{
+            console.log('invalid password');
+         }
+    }
+
 
     //check the balance
+    balance(acno,pswd){
+        if(this.validation(acno)){
+            if(this.accountDetails[acno].password == pswd){
+                console.log(`current balance of ${acno} is ${this.accountDetails[acno].balance}`);
+            }
+            else{
+                console.log('invalid password');
+            }
+        }
+        else{
+            console.log('invalid account number');
+        }
+    }
+
+
 
     //fund transfer
+    fundTransfer(fromAcno, ToAcno, fpswd, amount){
+        if(this.validation(fromAcno) && this.validation(ToAcno)){
+            if(this.accountDetails[fromAcno].password == fpswd){
+                if(this.accountDetails[fromAcno].balance >= amount){
+                    console.log(`current balance of ${fromAcno} is ${this.accountDetails[fromAcno].balance} and ${ToAcno} is ${this.accountDetails[ToAcno].balance} before fund transfer`);
+                    this.accountDetails[fromAcno].balance -= amount
+                    this.accountDetails[ToAcno].balance += amount
+                    console.log(`current balance of ${fromAcno} is ${this.accountDetails[fromAcno].balance} and ${ToAcno} is ${this.accountDetails[ToAcno].balance} after fund transfer`);
+                    
+                }
+                else{
+                    console.log('insufficient balance');
+                }
+            }else
+            console.log('invalid password');
+        }
+        else{
+            console.log('invalid account number');
+        }
+    }
+
+
+
+
+
 }
 
+console.log('-----//validate a given account number--------');
+
 const user1 = new Bank()
-console.log(user1.validation(1001));
+console.log(user1.validation(1001)?'valid':'invalid');
+
+console.log('----//authenticate the account-------');
+
+user1.authentice(1000,'userone')
+
+console.log('-----//check the balance-----');
+
+user1.balance(1003,'userfour')
+
+console.log('----//fund transfer---');
+
+user1.fundTransfer(1000,1001,'userone',100)
