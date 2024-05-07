aaddcec441570a32463769fbc58c74d1c1c7476f
princeabrahams000 <princeabrahams000@gmail.com>
2024-05-08 00:16:58 +0530

    8/5-12.16AM

---
+++ JavaScript/prog87.js
@@ -0,0 +1,15 @@
+exp = 10*2+5
+
+//eval - predefined function to evaluate the expression
+try{
+    result = eval(exp)
+console.log(result);
+}
+
+catch{
+    console.log('check the eval expression');
+}
+
+finally{
+console.log('task completed');
+}
