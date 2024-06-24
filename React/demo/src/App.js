da470911c973ac45c46d5a82a56eba4ff8ea024f
princeabrahams000 <princeabrahams000@gmail.com>
2024-06-24 23:27:55 +0530

    React1

--- React/demo/src/App.js
+++ React/demo/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import myStyle from '../src/myStyle.module.css'
 
 function App() {
+
+  const h1style = {fontSize:'70px', color:'green', backgroundColor:'yellow'}
+
+  const subject = 'REACT'
+  const username = 'MAX WELL'
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    
+      <h1 style={h1style}>My first React Application.....</h1>
+      <h1 className={myStyle.reactStyle}>using module style eg1</h1>
+      <h2 className={myStyle.style2}>using module style eg2</h2>
+      <h1 className={[myStyle.style2,myStyle.style3].join(' ')}>join style2 and 3</h1>
+
+      <h1>My subject is:{subject}</h1>
+      <h2>My user name is: {username}</h2>
+
     </div>
   );
 }
