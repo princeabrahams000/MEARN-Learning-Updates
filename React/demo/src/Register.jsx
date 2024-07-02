d251770820b2800ad95981154051a974e2b39166
princeabrahams000 <princeabrahams000@gmail.com>
2024-07-02 15:08:35 +0530

    july2/2

--- React/demo/src/Register.jsx
+++ React/demo/src/Register.jsx
@@ -1,23 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Register() {
+
+    const [Details, setDetails] = useState({
+        username: "",
+        email: "",
+        password: ""
+    })
+
+    console.log(Details);
+
+    const handleRegister = () =>{
+        const{username,email,password} = Details
+        alert(`user registered successfully with user name is ${username} and Email as ${email}and password as ${password} `)
+    }
+
   return (
     <div className='d-flex justify-content-center align-items-center'>
         <form className='m-4 p-3 shadow w-50'>
             <h3 className='text-center text-primary '>Register</h3>
             <div className='mb-3'>
-                <input type="text" className='form-control' placeholder='Enter the username' />
+                <input type="text" className='form-control' placeholder='Enter the username' onChange={(e)=>setDetails({...Details,username:e.target.value
+                })} />
             </div>
             
             <div className='mb-3'>
-                <input type="email" className='form-control' placeholder='Enter the Email' />
+                <input type="email" className='form-control' placeholder='Enter the Email' onChange={(e)=>setDetails({...Details,email:e.target.value})} />
             </div>
 
             <div className='mb-3'>
-                <input type="text" className='form-control' placeholder='Enter the password'/>
+                <input type="text" className='form-control' placeholder='Enter the password' onChange={(e)=>setDetails({...Details,password:e.target.value})}/>
             </div>
             <div className='mb-3'>
-                <button className='btn btn-primary'> Register
+                <button className='btn btn-primary' onClick={handleRegister}> Register
                 </button>
             </div>
         </form>
